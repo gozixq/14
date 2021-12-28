f162dbd82465d7dfe7efe30e4097d9055c608c2c
gozixq <natsinee2545@gmail.com>
2021-12-28 16:13:42 +0700

    ppp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,21 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double A[],int N, double B[]){
+    double AM=0,SD=0,GM=1,HM=0,MX=A[0],MN=A[0];
+    for(int i=0;i<N;i++)
+    {
+        AM += A[i];
+        SD += A[i]*A[i];
+        GM *= A[i];
+        HM += (1/A[i]);
+        if(A[i] > MX) MX = A[i];
+        if(A[i] < MN) MN = A[i];
+    } 
+    B[0] = AM/N;
+    B[1] = sqrt((SD/N)-pow(B[0],2));
+    B[2] = pow(GM,pow(N,-1));
+    B[3] = N/HM;
+    B[4] = MX;
+    B[5] = MN;
+}
